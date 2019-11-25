c2967b511946eceea51140676d49a1df3d1d23ee
andrew (from workstation) <andrew@arch>
2019-11-25 15:20:14 +0100

    new walker patch

--- src/_patches.py
+++ src/_patches.py
@@ -1,4 +1,3 @@
-import stat
 import dulwich.diff_tree
 import dulwich.objects
 import dulwich.walk
@@ -8,34 +7,28 @@ import dulwich.errors
 # noinspection PyProtectedMember
 def patch_all():
 
-    # noinspection PyProtectedMember
-    def walk_trees_patched(store, tree1_id, tree2_id, prune_identical=False):
-        mode1 = tree1_id and stat.S_IFDIR or None
-        mode2 = tree2_id and stat.S_IFDIR or None
+    walk_trees_real = dulwich.diff_tree.walk_trees
 
-        todo = [
-            (dulwich.objects.TreeEntry(b'', mode1, tree1_id),
-             dulwich.objects.TreeEntry(b'', mode2, tree2_id))
-        ]
+    class Getter:
+        def __init__(self, inst):
+            self.inst = inst
 
-        while todo:
-            entry1, entry2 = todo.pop()
-            is_tree1 = dulwich.diff_tree._is_tree(entry1)
-            is_tree2 = dulwich.diff_tree._is_tree(entry2)
-            if prune_identical and is_tree1 and is_tree2 and entry1 == entry2:
-                continue
-
+        def __getitem__(self, item):
             try:
-                tree1 = is_tree1 and store[entry1.sha] or None
-                tree2 = is_tree2 and store[entry2.sha] or None
-            except KeyError as error:
-                print("skipped tree", error.args[0])
-                tree1 = None
-                tree2 = None
+                return self.inst[item]
 
-            path = entry1.path or entry2.path
-            todo.extend(reversed(dulwich.diff_tree._merge_entries(path, tree1, tree2)))
-            yield entry1, entry2
+            except KeyError:
+                print("skipped", item)
+
+    def walk_trees_patched(store, *args, **kwargs):
+        generator = walk_trees_real(Getter(store), *args, **kwargs)
+
+        while True:
+            try:
+                yield next(generator)
+
+            except StopIteration:
+                break
 
     dulwich.diff_tree.walk_trees = walk_trees_patched
 
